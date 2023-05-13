1c47280c7c85063401b72efa1d43833a6bc0ea3f
Egg <88744505+developer-egg@users.noreply.github.com>
2023-05-13 09:51:36 -0400

    Added documentation to the window class

--- jelly/window.py
+++ jelly/window.py
@@ -2,6 +2,19 @@ import sys
 import sdl2.ext
 
 class Window:
+    """
+    Instantiate a game window.
+
+    :param title: The title of the window.
+    :type title: str
+
+    :param width: The width of the window.
+    :type width: int
+
+    :param height: The height of the window.
+    :type height: int
+    """
+
     def __init__(self, title, width, height):
         self.width = width
         self.height = height

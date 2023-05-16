7e404c874e5a4c158fd8a7a20922057a5671bbf8
Egg <88744505+developer-egg@users.noreply.github.com>
2023-05-16 14:44:31 -0400

    Improved the rect class to allow unfilled rectangles

--- jelly/rect.py
+++ jelly/rect.py
@@ -1,5 +1,5 @@
 import sdl2.ext
-
+import sdl2.sdlgfx
 class Rect:
     """
     Instantiate an object that draws a rectangle on the window.
@@ -7,28 +7,35 @@ class Rect:
     :param window: The window object.
     :type window: SDL_Window
 
-    :param x: The x position of the top-left corner of the rectangle.
-    :type x: int
+    :param x1: The x position of the top-left corner of the rectangle.
+    :type x1: int
 
-    :param y: The y position of the top-left corner of the rectangle.
-    :type y: int
+    :param y1: The y position of the top-left corner of the rectangle.
+    :type y1: int
 
-    :param width: The width of the rectangle.
-    :type width: int
+    :param x2: The x position of the bottom-right corner of the rectangle.
+    :type x2: int
 
-    :param height: The height of the rectangle.
-    :type height: int
+    :param y2: The y position of the bottom-right corner of the rectangle.
+    :type y2: int
+
+    :param isFilled: Determines if the rectangle is filled or not.
+    :type isFilled: bool
 
     :param color: The r, g, b values of the color of the rectangle.
     :type color: tuple
     """
 
-    def __init__(self, window, x, y, width, height, color=(255, 255, 255)):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+    def __init__(self, window, x1, y1, x2, y2, isFilled, color=(255, 255, 255)):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2= x2
+        self.y2 = y2
         self.color = color
         self.window = window
+        self.isFilled = isFilled
 
-        sdl2.ext.draw.fill(window.sdl_window.get_surface(), sdl2.ext.Color(self.color[0], self.color[1], self.color[2]), (self.x, self.y, self.width, self.height))
+        if isFilled:    
+            sdl2.sdlgfx.boxColor(window.renderer, self.x1, self.y1, self.x2, self.y2, sdl2.ext.Color(self.color[0], self.color[1], self.color[2]))
+        else:
+            sdl2.sdlgfx.rectangleColor(window.renderer, self.x1, self.y1, self.x2, self.y2, sdl2.ext.Color(self.color[0], self.color[1], self.color[2]))

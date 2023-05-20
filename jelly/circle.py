22129259ddee3082af2989dd722b4e8b316e7c41
maytees <mateespublicprofile@gmail.com>
2023-05-20 16:44:24 -0400

    fmt & fix docs

--- jelly/circle.py
+++ jelly/circle.py
@@ -16,14 +16,17 @@ import sdl2.sdlgfx
     :type radius: int
 
     :param isFilled: Determines if the circle is filled or not.
-    :type isFilled: int
+    :type isFilled: bool
 
     :param color: The r, g, b values of the color of the circle.
     :type color: tuple
     """
 
+
 class Circle:
-    def __init__(self, window, x, y, radius, isFilled=True, opacity=100, color=(0, 0, 0)):
+    def __init__(
+        self, window, x, y, radius, isFilled=True, opacity=100, color=(0, 0, 0)
+    ):
         self.window = window
         self.x = x
         self.y = y
@@ -31,6 +34,17 @@ class Circle:
         self.isFilled = isFilled
         self.color = color
         self.opacity = round(255 * (opacity / 100))
-        
-        params = (window.renderer.renderer, x, y, radius, self.color[0], self.color[1], self.color[2], opacity)
-        sdl2.sdlgfx.filledCircleRGBA(*params) if isFilled else sdl2.sdlgfx.aacircleRGBA(*params)
+
+        params = (
+            window.renderer.renderer,
+            x,
+            y,
+            radius,
+            self.color[0],
+            self.color[1],
+            self.color[2],
+            opacity,
+        )
+        sdl2.sdlgfx.filledCircleRGBA(*params) if isFilled else sdl2.sdlgfx.aacircleRGBA(
+            *params
+        )
